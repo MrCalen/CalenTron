baac21fbde84169acd5431aaa8f508862f71a003
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-08-04 01:33:49 +0200

    electron: add transport widget

---
+++ electron/views/js/widgets/TramWidget.js
@@ -0,0 +1,42 @@
+app.directive('tramWidget', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'tramWidget.html',
+        controller: function ($scope, $http, $timeout) {
+
+            $scope.size = 'small';
+            $scope.access_token = localStorage.getItem('access_token');
+            $scope.url = window.url;
+
+            $scope.fetchData = function () {
+                $timeout(function () {
+                    $scope.fetchTramway();
+                    $scope.fetchSubway();
+                    $scope.fetchData();
+                }, 120000); // 2 min
+            };
+
+            $scope.fetchTramway = function () {
+                $scope.loadingTramway = true;
+                $http.get($scope.url + "/api/tram?token=" + $scope.access_token)
+                .success(function (data) {
+                    $scope.tram = data;
+                    $scope.loadingTramway = false;
+                });
+            };
+            $scope.fetchSubway = function () {
+                $scope.loadingSubway = true;
+                $http.get($scope.url + "/api/subway?token=" + $scope.access_token)
+                .success(function (data) {
+                    $scope.subway = data;
+                    $scope.loadingSubway = false;
+                });
+            };
+
+            $scope.fetchTramway();
+            $scope.fetchSubway();
+            $scope.fetchData();
+
+        }
+    };
+});
