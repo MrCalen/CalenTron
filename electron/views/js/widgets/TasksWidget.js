09068e88bb5759993f5159c0808f7d51407dacd6
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-08-03 20:52:45 +0200

    electron: solve or unsolve task

--- electron/views/js/widgets/TasksWidget.js
+++ electron/views/js/widgets/TasksWidget.js
@@ -3,16 +3,36 @@ app.directive('tasksWidget', function () {
         restrict: 'E',
         templateUrl: "taskWidget.html",
         controller: function ($scope, $http, $timeout) {
+            $scope.size = 'small';
+
             var access_token = localStorage.getItem('access_token');
 
+            $scope.loading = true;
             $scope.refresh = function () {
                 $http.get(window.url + "/api/tasks?token=" + access_token)
-                .then(function () {
-                    console.log("coucou les potes");
+                .then(function (response) {
+                    $scope.tasks = response.data;
+                    $scope.loading = false;
                 });
             };
 
+            function refreshData() {
+                $timeout(function () {
+                    refresh();
+                    refreshData();
+                }, 2000);
+            }
             $scope.refresh();
+
+            $scope.updateTask = function (task) {
+                $http.post(window.url + "/api/task/" + task.id
+                    + "/" + (task.solved ? 'solve' : 'unsolve')
+                    + '?token=' + $scope.access_token)
+                    .success(function (data) {
+                       console.log(data);
+                        $scope.refresh();
+                    });
+            }
         }
     };
 });
