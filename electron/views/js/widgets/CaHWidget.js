846c8fe2bbb0254c4af24af249d97c523b128944
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-08-04 05:43:07 +0200

    electron: add last cah comic

---
+++ electron/views/js/widgets/CaHWidget.js
@@ -0,0 +1,19 @@
+app.directive('cahWidget', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'cahWidget.html',
+        controller: function ($scope, $http) {
+
+            var access_token = localStorage.getItem('access_token');
+            $scope.fetch = function () {
+                $http.get(window.url + "/api/cah?token=" + access_token)
+                .success(function (data) {
+                    console.log(data);
+                    $scope.img = data.result;
+                });
+            };
+
+            $scope.fetch();
+        }
+    };
+});
