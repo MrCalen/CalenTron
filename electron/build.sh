d9d82d4f0bae5f6ac1c7a1bd38535db52ccfdb14
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-08-05 11:05:25 +0200

    build: improve execution script

--- electron/build.sh
+++ electron/build.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 
-electron-packager ./ --platform=darwin --arch=x64
+set -e
+
+APP=/Applications/calen.app
+
+rm -rf $APP
+electron-packager ./ --platform=darwin --arch=x64 --overwrite
+mv ./calen-darwin-x64/calen.app $APP
