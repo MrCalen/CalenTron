ff1f373015d9084a23425f040bdc5604f6f93035
Pierre-Louis Dagues <redis@acu.epita.fr>
2016-10-13 11:15:04 +0200

    deploy: update with installation

--- electron/build.sh
+++ electron/build.sh
@@ -5,5 +5,12 @@ set -e
 APP=/Applications/calen.app
 
 rm -rf $APP
+echo "Fetching electron node dependencies"
+npm install
+echo "Fetching application node dependencies"
+tmp=$(cd views && npm install)
+echo "Fetching bower dependencies"
+bower install
+
 electron-packager ./ --platform=darwin --arch=x64 --overwrite
-mv ./calen-darwin-x64/calen.app $APP
+cp -r ./calen-darwin-x64/calen.app $APP
