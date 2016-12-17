e28ed97f540b1ccb3b0a2ed6cc13d1eb3209676b
Pierre-Louis Dagues <redis@acu.epita.fr>
2016-12-17 04:31:01 +0100

    ci: add more tests

--- docker/backend-src/controllers/PingController.js
+++ docker/backend-src/controllers/PingController.js
@@ -2,7 +2,6 @@ exports.getPing = function () {
     return new Promise(function (resolve, error) {
         var tcpp = require('tcp-ping');
         tcpp.ping({ address: global.config.server.ping_url }, function(err, data) {
-            console.log(err);
             resolve(data);
         });
     });
--- docker/backend-src/test/main.js
+++ docker/backend-src/test/main.js
@@ -36,7 +36,6 @@ describe('API', function () {
         var result = null;
 
         before(function (done) {
-            this.timeout(5000);
             var ctrl = require('../controllers/PingController');
             ctrl.getPing()
             .then(function (res) {
@@ -51,8 +50,46 @@ describe('API', function () {
 
         it('Result - Attempts', function () {
             assert(result.attempts, 10);
-        })
+        });
 
+        it('Result - Addr', function () {
+            assert(result.address == global.config.server.ping_url);
+        });
+
+        describe('Results - Analysis', function () {
+            it('Type', function () {
+                should(result.results).be.an.Array();
+            });
+
+            it('Number', function () {
+                assert(result.results.length == result.attempts);
+            });
+        });
     });
+
+    describe('WeatherController', function () {
+        var result = null;
+
+        before(function (done) {
+            var ctrl = require('../controllers/WeatherController');
+            ctrl.getWeather()
+                .then(function (res) {
+                    result = res;
+                    done();
+                });
+        });
+
+        it('Result - Ok', function () {
+            should(result).be.an.Object();
+        });
+
+        it('Result - Api Response', function () {
+            assert(result.cod === '200');
+        });
+
+        it('Result - Typing', function () {
+            should(result.list).be.an.Array();
+        })
+    })
 
 });
